41149c409083b73f42334ee6ea3b444e75aa70f9
Arnav Kumar <arnavkumar@rippling.com>
2023-01-27 02:13:00 +0530

    update test code

--- test_python_code_for_documentation.py
+++ test_python_code_for_documentation.py
@@ -2,13 +2,16 @@
 Example docstring for generating documentation
 
 1. **First** - Enclose word in double asterisks for bold
-2. **Second** - For numbered points
+2. **Second** - Use numbers for points
 """
 
 # === print ===
 def print_text(text="hello world"):
     print(text)
-
+    """
+    A method to print text or hello world
+    """
+    
 # === generator ===
 def ListGenerator(num):
     """
